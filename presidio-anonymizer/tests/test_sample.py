3d382a3633a67fc04b1020497502770a9f15476d
Alex Richardson <231764980+Aricha63@users.noreply.github.com>
2025-09-30 18:48:27 +0000

    Added test case to ensure the output structure is correct

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -2,5 +2,4 @@ import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
-    # replace the following line with your test
-    pass
+    assert str(sample_run_anonymizer("My name is Bond.", 11, 15)) == "text: My name is BIP.\nitems:\n[\n    {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}\n]\n"
