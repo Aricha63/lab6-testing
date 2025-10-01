aac3cf5531f82dee3a8fc8d93491a4125e626b44
Alex Richardson <231764980+Aricha63@users.noreply.github.com>
2025-10-01 02:08:09 +0000

    Changed variable name "bond_test" to "result" in test_sample.py

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -2,9 +2,9 @@ import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
-    bond_test = sample_run_anonymizer("My name is Bond.", 11, 15)
+    result = sample_run_anonymizer("My name is Bond.", 11, 15)
     
-    assert bond_test.text == "My name is BIP."
-    assert len(bond_test.items) == 1
-    assert bond_test.items[0].start == 11
-    assert bond_test.items[0].end == 14
+    assert result.text == "My name is BIP."
+    assert len(result.items) == 1
+    assert result.items[0].start == 11
+    assert result.items[0].end == 14
